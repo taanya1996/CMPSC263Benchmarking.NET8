858b9deb684da3605a51262120a3593393a486a5
Taanya Nithya Anand <taanya.mys@gmail.com>
2024-02-26 13:41:44 -0800

    Dynamic PGO - Scalable Approximate Counting + GDV

---
+++ dynamic-pgo-gdv/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Configs;
+
+BenchmarkRunner.Run<Benchmarks.Benchmark>();
+
+namespace Benchmarks{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net70)]
+    [SimpleJob(RuntimeMoniker.Net80)]
+    public class Benchmark{
+        
+        internal interface ICalculator{
+            int Calculate(int a, int b);
+        }
+
+        public class Calculator_Imp : ICalculator{
+            public int Calculate(int a, int b) => a + b;
+        }
+
+        private ICalculator _calculator;
+        private int a =2;
+        private int b=3;
+
+        [GlobalSetup]
+        public void Setup() => _calculator = new Calculator_Imp();
+
+        [Benchmark]
+        public int Calculate() => _calculator.Calculate(a,b);
+    }
+
+}
---
+++ dynamic-pgo-scalable-approximate-counter/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+
+public class ScalableAppCounter{
+    
+    private static uint counter = 0;
+    private const int ItersPerThread = 100_000_000;
+
+    public static void Main(){ 
+        int j = 0;               
+        while (j<5)
+        {
+            Run("Interlock", _ => { for (int i = 0; i < ItersPerThread; i++) Interlocked.Increment(ref counter); });
+            Run("Racy     ", _ => { for (int i = 0; i < ItersPerThread; i++) counter++; });
+            Run("Scalable ", _ => { for (int i = 0; i < ItersPerThread; i++) Count(ref counter); });
+            Console.WriteLine();
+            j++;
+        }
+    }
+
+    private static void Run(string name, Action<int> body){
+        counter = 0;
+        long start = Stopwatch.GetTimestamp();
+        Parallel.For(0, Environment.ProcessorCount, body);
+        long end = Stopwatch.GetTimestamp();
+        Console.WriteLine($"{name} => Expected: {Environment.ProcessorCount * ItersPerThread:N0}, Actual: {counter,13:N0}, Elapsed: {Stopwatch.GetElapsedTime(start, end).TotalMilliseconds}ms");
+    }
+
+    
+    private static void Count(ref uint sharedCounter){
+        uint currentCount = sharedCounter, delta = 1;
+        if (currentCount > 0)
+        {
+            int logCount = 31 - (int)uint.LeadingZeroCount(currentCount);
+            if (logCount >= 13)
+            {
+                delta = 1u << (logCount - 12);
+                uint random = (uint)Random.Shared.NextInt64(0, uint.MaxValue + 1L);
+                if ((random & (delta - 1)) != 0)
+                {
+                    return;
+                }
+            }
+        }
+
+        Interlocked.Add(ref sharedCounter, delta);
+    }
+
+} 
+
+
+
