69dc36137d9d8841cc37e2a4f794ba893cf404c8
Swathi S Bhat <16900977+SwathiSBhat@users.noreply.github.com>
2024-02-26 13:13:51 -0800

    Added benchmark for parallel forasync vs foreachasync

---
+++ parallel-forasync/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Configs;
+
+BenchmarkRunner.Run<Benchmarks.Benchmark>();
+
+namespace Benchmarks
+{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net80)]
+    [Config(typeof(Config))]
+    public class Benchmark
+    {
+        public class Config : ManualConfig
+		{
+			public Config() => AddExporter(RPlotExporter.Default);
+		}
+
+        [Benchmark(Baseline = true)]
+        public Task ForEachAsyncTest() => Parallel.ForEachAsync(Enumerable.Range(0, 100), async (i, count) => await Task.Delay(1));
+
+        [Benchmark]
+        public Task ForAsyncTest() => Parallel.ForAsync(0, 100, async (i, count) => await Task.Delay(1));
+    }
+}
