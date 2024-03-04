23029e4e57b935f1f1b5e8f72a1171d6f89495d1
Taanya Nithya Anand <taanya.mys@gmail.com>
2024-03-03 23:59:42 -0800

    branching - redundant optimization & cond instruction generation

---
+++ branching-conditional-inst-generation/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+
+BenchmarkRunner.Run<Benchmarks.Benchmark>();
+
+namespace Benchmarks{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net70)]
+    [SimpleJob(RuntimeMoniker.Net80)]
+    [HideColumns("Error", "StdDev",  "Median", "RatioSD")]
+    [DisassemblyDiagnoser]
+    public class Benchmark{
+        private static readonly Random s_rand = new();
+        /*        
+        class Dummy{
+            public static int None;
+            public static int GetRandom1(){
+                return  s_rand.Next(0, 101);
+            }
+        }
+        
+
+        [Benchmark]
+        public int CondInstEmissionTest()=> CondInstEmission( s_rand.NextDouble() < Probability);
+        public int CondInstEmission(bool val)=> val ? Dummy.GetRandom1(): Dummy.None;
+        */
+
+        [Benchmark]
+        public int GetMax(){
+            int n1 = s_rand.Next(0, 101);
+            int n2 = s_rand.Next(0, 101);
+            int ans = n1>n2 ? n1: n2;
+            return ans;
+        }
+    }
+}
---
+++ branching-redundant-branch-optimization/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
+
+BenchmarkRunner.Run<Benchmarks.Benchmark>();
+
+namespace Benchmarks{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net70)]
+    [SimpleJob(RuntimeMoniker.Net80)]
+    [HideColumns("Error", "StdDev",  "Median", "RatioSD")]
+    [DisassemblyDiagnoser]
+    public class Benchmark{
+        private static readonly Random s_rand = new();
+
+        [Benchmark]
+        public void BranchPrediction(){
+            int val;
+            int rand_int = s_rand.Next(0, 101);
+            if(rand_int<=100){
+                if(rand_int<=100){
+                    val = 1;
+                }
+                else{
+                    val = 0;
+                }
+            }
+            else{
+                if(rand_int<=100){
+                    val = 1;
+                }
+                else{
+                    val = 0;
+                }
+            }
+            return;
+        }
+
+    }
+
+}
